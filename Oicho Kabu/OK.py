08c56e213d0c5545a274238a0bc6c3723b8f0fba
MegaZeroX <mfmegazerox@gmail.com>
2019-07-23 10:09:27 -0700

    Accidentally forgot to delete the code

--- Oicho Kabu/OK.py
+++ Oicho Kabu/OK.py
@@ -1,8 +1,1 @@
 #create a function called "whoWins" which takes in 3 player cards and 3 dealer card values, and decide who wins the match
-def whoWins(playerCardOne, playerCardTwo, playerCardThree, dealerCardOne, dealerCardTwo, dealerCardThree):
-    playerSum = playerCardOne + playerCardTwo + playerCardThree
-    dealerSum = dealerCardOne + dealerCardTwo + dealerCardThree
-    if(playerSum % 10 > dealerSum % 10):
-        print("The player wins")
-    else:
-        print("The dealer wins")
