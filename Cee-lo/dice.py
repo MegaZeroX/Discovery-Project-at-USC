60569b1b2fae8d541d4ddc053e60b19440f868cf
MegaZeroX <mfmegazerox@gmail.com>
2019-07-25 09:38:07 -0700

    Expanded the description

--- Cee-lo/dice.py
+++ Cee-lo/dice.py
@@ -1,1 +1,19 @@
 import random
+
+#Create a "Die" class with the following methods:
+#1) A constructor called __init__ that takes how many sides the die will have
+#2) A roll method, which gets the dice
+#3) A method called current_roll(), which should say what the last thing the die just rolled
+
+#After doing the above, try testing it out! Remember to ask if you have any questions about it!
+
+#Cool, let's try making a Dice class now!
+#We will take the number of dice and the number of sides each one should have in our constructor
+#I recommend using the Dice class you made and making a list of dice!
+#After the constructor, we should make the following methods:
+#1) A "roll_all" method which will roll all of the dice, and return a list of the numbers they output
+#2) A "reroll" method which takes the index of a die to reroll and rerolls that one, returning the result
+#3) A "current_roll" method which will return a list with all of the previous rolls of each die
+
+#Again, feel free to test it out and ask for help at any point along the way! When you complete this, you can try running
+#the "CeeLoGui" file to see if you successfully completed the game!
